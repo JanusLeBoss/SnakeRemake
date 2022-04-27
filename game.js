a370bcdb7e060ba1d89efbb643deb565f5bb9fda
JanusLeBoss <102878880+JanusLeBoss@users.noreply.github.com>
2022-04-27 17:16:03 +0200

    bowserpicture
    nous avons mis une image de bowser et fait un menu start/pause

--- game.js
+++ game.js
@@ -19,7 +19,10 @@ cell_size = 30;
 row_length = 20;  // number of cells in a row or a column
 tail = [];
 tail_length = 5;
-interval_id = undefined;
+interval_id = undefined
+headImg = new Image();
+headImg.src = './BowserCastré.png';
+var headPattern;
 
 window.onload=function() {
 	canv=document.getElementById("game_canvas");
@@ -27,27 +30,40 @@ window.onload=function() {
 	scoredisplay=document.getElementById("score");
 	finalscore=document.getElementById("end_score");
 	ctx=canv.getContext("2d");
+	headPattern = ctx.createPattern(headImg, 'no-repeat');
 	document.addEventListener("keydown",keyPush);
 	start_game();
 }
-
 function start_game(){
 	// boucle infinie : appelle la fonction game() toutes les 15/1000 de seconde
 	started = true;
 	interval_id = setInterval(game,1000/15);
 }
 
-function stop_game(){
+function stop_game(display_gameover=true){
+	started = false;
 	// si la boucle infinie tourne, alors on l'arrete et on affiche l'ecran game over
 	if(interval_id !== undefined){
 		// on arrete la boucle
 		clearInterval(interval_id);
 		interval_id = undefined;
-		// on uptade le score sur l'ecran game over
-		finalscore.textContent  = score;
-		// on affiche l'ecran game over
-		gameover.className = "visible";
+		if (display_gameover) {
+			// on uptade le score sur l'ecran game over
+			finalscore.textContent  = score;
+			// on affiche l'ecran game over
+			gameover.className = "visible";
+		}
 	}
+}
+
+function pause_game() {
+	stop_game(false);
+}
+unpause_game = start_game;
+
+function bowser_coordinates(number) {
+	// retourne les coordonnées d'un bowser dans le sprite des bowser 
+	return [number*53, 0, 50, 50]; // [x, y, width, height]
 }
 
 function move_snake(){
@@ -83,6 +99,12 @@ function paint_square(x, y){
 	ctx.fillRect(x * cell_size, y * cell_size, cell_size-2, cell_size-2);
 }
 
+function paint_head(x, y, number = 0){
+	ctx.drawImage(headImg,
+	...bowser_coordinates(number),
+	(x-0.35) * cell_size, (y-0.35) * cell_size, cell_size*1.7, cell_size*1.7);
+}
+
 function game() {
 	// partie 1 : avancer le serpent
 	move_snake();
@@ -106,11 +128,12 @@ function game() {
 		}
 	}
 
-	// partie 4 : Render
+	// partie 4 : Affiche
 	paint_canvas_background();
 	// affiche le serpent
-	ctx.fillStyle="#d7b024";
-	for(var i=0;i<tail.length;i++) {
+	paint_head(x_position, y_position); // on affiche la tete 
+	ctx.fillStyle = "#d7b024";
+	for(var i=0;i<tail.length;i++) { // on affiche la queue 
 		paint_square(tail[i].x, tail[i].y);
 	}
 	// affiche la pomme
@@ -145,46 +168,13 @@ function keyPush(evt) {
 		case 83: // S
 			if(y_speed==0) {x_speed=0;y_speed=1;}
 			break;
+		case 27: // echap
+			if (started) {
+				pause_game();
+			} else {
+				unpause_game();
+			}
 		default: // debug mode, log key code for yet unmapped keys
 			if(debug) { console.log(evt.keyCode); }
 	}
 }
-
-function animate() {
-
-}
-/*const img = new image();
-img.onload = start;
-img.src = 'https://i.imgur.com/fqgm8uh.png';
-
-function start() {
-  const ctx = document.queryselector('canvas').getcontext('2d');
-  
-  const pattern = ctx.createpattern(img, 'repeat');
-
-  function render(time) {
-    time *= 0.001;  // seconds;
-    
-    ctx.clearrect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    
-    const x = math.sin(time * 1.1) * 150 + 150;
-    const y = math.sin(time * 1.2) * 50 + 50;
-    
-    ctx.translate(x, y);
-	// peindre avec le motif
-    ctx.fillstyle = pattern;
-	// on dessinne le carr? 
-    ctx.fillrect(0, 0, 30, 40);
-	// on dessinne le cercle
-    ctx.beginpath();
-    ctx.arc(0, 0, 25, 0, math.pi * 2);
-    ctx.fill();
-    
-    ctx.settransform(1, 0, 0, 1, 0, 0);  // set it back to the default
-    
-    requestanimationframe(render);
-  }
-  requestanimationframe(render);
-}
-*/
-
